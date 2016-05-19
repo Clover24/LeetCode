ab3533eedfc81cbe489be050c4d4961c2e93492d
caiying_24 <caiying_24@foxmail.com>
2016-05-19 09:21:52 +0800

    005

--- LeetCode_005-Longest-Palindromic-Substring/LongestPalindromicSubstring.h
+++ LeetCode_005-Longest-Palindromic-Substring/LongestPalindromicSubstring.h
@@ -53,3 +53,32 @@ public:
 		return begin + 1;
 	}
 };
+
+class Solution56 {
+public:
+	string longestPalindrome(string s) {
+	    if(s.length() <= 1) return s;
+		string res;
+		for (int i = 0; i < s.length() - 1; ++i) {
+		    string s1, s2;
+			s1 = longestPalindromeCore(s, i, i + 1);
+			s2 = longestPalindromeCore(s, i , i);
+			if (res.length() < s1.length()) 
+			    res = s1;
+			if (res.length() < s2.length()) 
+			    res = s2;
+			if(res.length() == s.length())
+			    break;
+		}
+		return res;
+	}
+	string longestPalindromeCore(string &s, int begin, int end) {
+		while (begin >= 0 && end < s.length()) {
+		    if(s[begin] != s[end]) 
+		        break;
+			begin--;
+			end++;
+		}
+		return s.substr(begin + 1, end - 1 - begin);
+	}
+};
