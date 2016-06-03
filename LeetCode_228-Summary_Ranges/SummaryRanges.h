5fb66280fab930b54268c3ec1b3661d1bfa5ea11
caiying_24 <caiying_24@foxmail.com>
2016-06-03 14:35:45 +0800

    M:091&228&268

--- LeetCode_228-Summary_Ranges/SummaryRanges.h
+++ LeetCode_228-Summary_Ranges/SummaryRanges.h
@@ -4,7 +4,8 @@
 using namespace std;
 
 /*
-228. Summary RangesDifficulty: Medium
+228. Summary Ranges
+Difficulty: Medium
 Given a sorted integer array without duplicates, return the summary of its ranges.
 For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
 */
